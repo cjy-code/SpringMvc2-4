44c2fdff8588ecada2aadc239670c39373ec15ae
jychoi <ggozxc@gmail.com>
2022-09-15 00:19:53 +0900

    validation practice ControllerV3  - bean Validation  groups 적용

--- main/java/hello/itemservice/domain/item/Item.java
+++ main/java/hello/itemservice/domain/item/Item.java
@@ -9,15 +9,17 @@ import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
-
+    @NotNull(groups = UpdateCheck.class) //수정시에만 적용
     private Long id;
-    @NotBlank(message = "공백은 입력할 수 없습니다.")
+
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class,
+            UpdateCheck.class})
     private Integer price;
-    @NotNull
-    @Max(9999)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = SaveCheck.class) //등록시에만 적용
     private Integer quantity;
     public Item() {
     }
